a16988d1c5251f861b2e7d1a54afcb200ccaa3a9
Roman Bärtschi <50102152+namorDev@users.noreply.github.com>
2023-12-20 09:56:20 +0100

    Add Temperature Sensor Tests with TDD

--- test/TemperatureSensor_test.cpp
+++ test/TemperatureSensor_test.cpp
@@ -10,29 +10,47 @@ using namespace ::testing;
 
 // add setup and teardown
 
-#if 0
+
 TEST(TemperatureSensorTest, get_temperature_mocked) 
 {
-  //AdcMock mockAdc;
-  //EXPECT_CALL(mockAdc, readAdcValue()).WillOnce(Return(20.0));
-  //TemperatureSensor tSens(mockAdc);
+  AdcMock mockAdc;
+  EXPECT_CALL(mockAdc, readAdcValue()).WillOnce(Return(20.0));
+  TemperatureSensor tSens(mockAdc);
 
-  //EXPECT_EQ(43, tSens.getTemperature());
+  EXPECT_EQ(43, tSens.getTemperature());
+}
+
+TEST(TemperatureSensorTest, get_temperature_mocked1) 
+{
+  AdcMock mockAdc;
+  EXPECT_CALL(mockAdc, readAdcValue()).WillOnce(Return(-14));
+  TemperatureSensor tSens(mockAdc);
+
+  EXPECT_EQ(-25, tSens.getTemperature());
+}
+
+TEST(TemperatureSensorTest, get_negativetemperature_mocked) 
+{
+  AdcMock mockAdc;
+  EXPECT_CALL(mockAdc, readAdcValue()).WillOnce(Return(-4.0));
+  TemperatureSensor tSens(mockAdc);
+
+  EXPECT_EQ(-5, tSens.getTemperature());
 }
 
 TEST(TemperatureSensorTest, get_temperature_stubbed) 
 {
-  //AdcStub stubAdc;
-  //TemperatureSensor tSens(stubAdc);
+  AdcStub stubAdc;
+  TemperatureSensor tSens(stubAdc);
 
-  //EXPECT_EQ(13, tSens.getTemperature());
+  EXPECT_EQ(23, tSens.getTemperature());
 }
 
 /////////////////////////////////////////////
 // TDD example
 TEST(TemperatureSensorTest, isInRange_positiveTemperature) 
 {
-
+  
 
 }
 
@@ -41,4 +59,123 @@ TEST(TemperatureSensorTest, isInRange_negativeTemperature)
 
 
 }
-#endif
+
+TEST(TemperatureSensorTest, given_temperature_is_10_when_is_in_range_then_return_true) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 10;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(true, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_20_when_is_in_range_then_return_true) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 20;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(true, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_21_when_is_in_range_then_return_false) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 21;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(false, isInRange);
+  
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_49_when_is_in_range_then_return_false)
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 49;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(false, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_50_when_is_in_range_then_return_true) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 50;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(true, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_60_when_is_in_range_then_return_true) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 60;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(true, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_0_when_is_in_range_then_return_false) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 0;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(false, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_9_when_is_in_range_then_return_false) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = 9;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(false, isInRange);
+
+}
+
+TEST(TemperatureSensorTest, given_temperature_is_minus1_when_is_in_range_then_return_false) 
+{
+  AdcStub stubAdc;
+    //Given
+    TemperatureSensor tSens(stubAdc);
+    int input = -1;
+    //When
+    bool isInRange = tSens.isInRange(input);
+    // Then
+    EXPECT_EQ(false, isInRange);
+
+}
+
+
+
